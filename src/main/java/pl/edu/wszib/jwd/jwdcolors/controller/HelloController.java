873070b8e9b84f3ca15a424b510c40b856b0bba7
mkulak <mkulak@student.wszib.edu.pl>
2023-06-04 12:54:10 +0200

    Widok selektora kolorów

--- src/main/java/pl/edu/wszib/jwd/jwdcolors/controller/HelloController.java
+++ src/main/java/pl/edu/wszib/jwd/jwdcolors/controller/HelloController.java
@@ -1,4 +1,4 @@
-package pl.edu.wszib.jwd.jwdcolors.controllers;
+package pl.edu.wszib.jwd.jwdcolors.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
